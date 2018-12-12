ae3cde9f5555007b4c4f3d4061e74148eb5243e0
Min Seop Kim <mkim39@wustl.edu>
2018-12-11 21:00:18 -0600

    instagram api complete

--- custom.js
+++ custom.js
@@ -1,10 +1,10 @@
 $( document ).ready(function(){
   var userFeed = new Instafeed({
     get: 'user',
-    userId: '',
+    userId: '1627406569',
     limit: 12,
     resolution: 'standard_resolution',
-    accessToken: '',
+    accessToken: '1627406569.1677ed0.a7827ca37f4f4e1e900972d070412179',
     sortBy: 'most-recent',
     template: '<div class = "gallery"><a href="{{image}}" titl="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a>',
 
